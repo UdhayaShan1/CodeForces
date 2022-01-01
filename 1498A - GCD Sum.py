7818490f7129557f049c90cc7aa4770a7d28dfd0
Udhaya <39399745+UdhayaShan1@users.noreply.github.com>
2022-01-01 16:31:36 +0800

    Update 1498A - GCD Sum.py

--- 1498A - GCD Sum.py
+++ 1498A - GCD Sum.py
@@ -1,24 +1,24 @@
+import math
 n1 = int(input())
 list1 = []
 for i in range(n1):
     list1.append(int(input()))
-    
-k1 = 0
+
 b1 = False
+k1 = 0
 while b1==False:
-    list2 = []
-    ref_int = list1[k1]
-    set2 = {}
-    for i in range(1,31622+1):
-        x = i **2
-        y = i **3
-        if x > ref_int and y >ref_int:
-            break
-        set2.add(x)
-        set2.add(y)
-        
-    print(len(set2))
-    k1+=1
     if k1==len(list1):
-        break
-        
+        break  
+    c1 = False
+    
+    ref_int = list1[k1]
+    temp1 = ref_int
+    while c1==False:
+        ref_sum = 0
+        for i in str(temp1):
+            ref_sum+=int(i)
+        if math.gcd(temp1,ref_sum) > 1:
+            print(temp1)
+            break
+        temp1 += 1
+    k1+=1
