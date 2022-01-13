1daaf484f7460f1844c9689b3f7ce1b35606c88a
Udhaya <39399745+UdhayaShan1@users.noreply.github.com>
2022-01-14 00:28:53 +0800

    Create 1437A - Marketing Scheme.py

---
+++ 1437A - Marketing Scheme.py
@@ -0,0 +1,23 @@
+n1 = int(input())
+list1 = []
+for i in range(n1):
+    x = input().split()
+    temp = []
+    for j in x:
+        temp.append(int(j))
+    list1.append(temp)
+    
+k1 = 0
+b1 = False
+while b1==False:
+    ref_list = list1[k1]
+    l = ref_list[0]
+    r = ref_list[1]
+    flag = 0
+    if l*2 > r and l*2 <= r*2:
+        print("YES")
+    else:
+        print("NO")
+    k1+=1
+    if k1==len(list1):
+        break
