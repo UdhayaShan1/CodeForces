e4712a97900dd8a9347d2bb2bf64feaa46235ed3
Udhaya <39399745+UdhayaShan1@users.noreply.github.com>
2022-01-18 23:18:32 +0800

    Create 1216A - Prefixes.py

---
+++ 1216A - Prefixes.py
@@ -0,0 +1,22 @@
+n1 = int(input())
+str1 = list(input())
+count = 0
+str2 = ''
+for i in range(0,int(len(str1)/2)):
+    
+    
+    if str1[i*2] == "a" and str1[i*2+1] == "b":
+        continue
+    
+    elif str1[i*2] == "b" and str1[i*2+1] == "a":
+        continue
+    else:
+        if str1[i*2] == "a":
+            str1[i*2] = "b"
+            str1[i*2+1] = "a"
+        else:
+            str1[i*2] = "a"
+            str1[i*2+1] = "b"
+        count+=1
+print(count)
+print(''.join(str1))
