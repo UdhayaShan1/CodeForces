fe63617ce865afeaf7d254f986c15b6612fdcad5
Udhaya <39399745+UdhayaShan1@users.noreply.github.com>
2022-04-03 15:17:05 +0800

    Create 1312B - Bogosort.py

---
+++ 1312B - Bogosort.py
@@ -0,0 +1,21 @@
+n1 = int(input())
+list1 = []
+list2 = []
+for i in range(n1):
+    list1.append(int(input()))
+    temp = []
+    x = input().split()
+    for j in x:
+        temp.append(int(j))
+    list2.append(temp)
+ 
+k1 = 0
+b1 = False
+while b1==False:
+    n = list1[k1]
+    ref = list2[k1]
+    ref.sort()
+    print(*ref[::-1])
+    k1+=1
+    if k1==len(list1):
+        break
